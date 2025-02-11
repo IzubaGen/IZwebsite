9379c901f19a8dcbd4a5772780cfc4e4ceceb44c
DavidIrankunda <irankundadavid64@gmail.com>
2025-02-11 17:20:27 +0300

    updating

--- js/agency.js
+++ js/agency.js
@@ -1,47 +1,54 @@
 /*!
- * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').on('click', function(event) {
+        event.preventDefault();
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').on('click', function() {
+        if ($('.navbar-toggle').is(':visible')) {
+            $('.navbar-toggle').click();
+        }
     });
 });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-
-    
-
-
-    // Open Modal
+// Open Portfolio Modal
 document.querySelectorAll('.portfolio-link').forEach(link => {
     link.addEventListener('click', function (e) {
         e.preventDefault();
         const modalId = this.getAttribute('href');
         const modal = document.querySelector(modalId);
-        modal.classList.add('modal-open');
-        document.body.style.overflow = 'hidden'; // Prevent scrolling on the main page
+
+        if (modal) {
+            modal.classList.add('modal-open');
+            document.body.style.overflow = 'hidden'; // Prevent scrolling on the main page
+        }
     });
 });
 
-// Close Modal
+// Close Portfolio Modal
 document.querySelectorAll('.close-modal, .btn-primary').forEach(button => {
     button.addEventListener('click', function () {
         const modal = this.closest('.portfolio-modal');
-        modal.classList.remove('modal-open');
-        document.body.style.overflow = 'auto'; // Restore scrolling on the main page
-    
+
+        if (modal) {
+            modal.classList.remove('modal-open');
+            document.body.style.overflow = 'auto'; // Restore scrolling on the main page
+        }
+    });
+});
